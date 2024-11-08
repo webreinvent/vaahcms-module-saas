2ab57e1102cf9e58c01586f0ab6f9454f085641b
Himanshu Bamania <himanshu-b001@webreinvent.com>
2024-11-08 08:24:34 +0530

    basic server setup completed

--- VueThree/components/Aside.vue
+++ VueThree/components/Aside.vue
@@ -14,6 +14,11 @@ const items = ref([
         icon: 'fa-regular fa-chart-bar',
         route: "/"
       },
+        {
+            label: 'ServerV3',
+            icon: 'fa-regular fa-chart-bar',
+            route: "/serversv3"
+        },
     ]
   },
 ]);
--- VueThree/routes/routes.js
+++ VueThree/routes/routes.js
@@ -1,7 +1,9 @@
 let routes= [];
 
 import dashboard from "./vue-routes-dashboard";
+import serverv3 from "./vue-routes-serversv3";
 
+routes = routes.concat(serverv3);
 routes = routes.concat(dashboard);
 
 export default routes;
