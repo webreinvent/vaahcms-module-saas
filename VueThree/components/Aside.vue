e221aff204827e9729786a3cb980df6f6be8b44b
Atul Pratap Singh <atul-s001@webreinvent.com>
2024-11-12 11:14:18 +0530

    Updated: icon of aside app menu

--- VueThree/components/Aside.vue
+++ VueThree/components/Aside.vue
@@ -16,7 +16,7 @@ const items = ref([
       },
         {
             label: 'Apps',
-            icon: 'fa-regular fa-chart-bar',
+            icon: 'fa-brands fa-app-store',
             route: "/appsv3"
         },
     ]
